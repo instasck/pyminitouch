2a938e35174ee1ea3de68f0a30fb02840fa794d8
sionking <sionking@gmail.com>
2019-09-02 21:57:08 +0300

    round down

--- pyminitouch/actions.py
+++ pyminitouch/actions.py
@@ -188,7 +188,7 @@ class MNTDevice(object):
 
             # add delay between points
             if duration:
-                _builder.wait(randint(0, 0.1*duration)+duration)
+                _builder.wait(randint(0, int(0.1*duration))+duration)
             _builder.commit()
 
         _builder.publish(self.connection)
